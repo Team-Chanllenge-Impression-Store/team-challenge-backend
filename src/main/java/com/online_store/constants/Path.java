508403220838917deceb3141554951c4f23229e6
EconLQ <zavliybogdan@gmail.com>
2024-08-25 12:56:44 +0300

    updated security config with the request matchers for product and category

--- src/main/java/com/online_store/constants/Path.java
+++ src/main/java/com/online_store/constants/Path.java
@@ -1,14 +1,12 @@
 package com.online_store.constants;
 
 public class Path {
-    // TODO: check wtf with AuthEntryPoint when you add v1 to auth pathname
-    //    public static final String API_VERSION = "/v1";
-//    public static final String API_PATHNAME = "api" + API_VERSION;
-//    public static final String AUTH = API_PATHNAME + "/auth";
-    public static final String API_PATHNAME = "/api";
+    public static final String API_VERSION = "/v1";
+    public static final String API_PATHNAME = "/api" + API_VERSION;
     public static final String AUTH = API_PATHNAME + "/auth";
     public static final String PRODUCT = API_PATHNAME + "/product";
     public static final String ORDER = API_PATHNAME + "/order";
+    public static final String CATEGORY = API_PATHNAME + "/category";
 
     private Path() {
     }
