a02121ff084bff4e165625ce82e9fd58793ccb3f
EconLQ <zavliybogdan@gmail.com>
2024-09-15 19:21:40 +0300

    disabled cors and permit all http requests for testing

--- src/test/java/com/online_store/configuration/WebSecurityTestConfig.java
+++ src/test/java/com/online_store/configuration/WebSecurityTestConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,11 +17,10 @@ public class WebSecurityTestConfig {
     @Bean
     @Order(1)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/**").permitAll()
-                );
-        return http.build();
+        return http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .build();
     }
 }
