f9ff582114119e8a2b74c6a013ebb77df22a905e
EconLQ <zavliybogdan@gmail.com>
2024-10-09 16:39:59 +0300

    removed preAuth from methods

--- src/main/java/com/online_store/web/generator/controller/GeneratorController.java
+++ src/main/java/com/online_store/web/generator/controller/GeneratorController.java
@@ -6,37 +6,33 @@ import com.online_store.web.generator.service.ProductGeneratorService;
 import com.online_store.web.generator.service.UserGeneratorService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 @RequiredArgsConstructor
 @RequestMapping(Path.GENERATOR)
-@Tag(name = "Generator", description = "Generates Users, Products and Orders")
+@Tag(name = "Generator", description = "Generates random Users and Products")
 public class GeneratorController {
     private final UserGeneratorService userGeneratorService;
     private final ProductGeneratorService productGeneratorService;
 
-    @GetMapping("/users")
-    @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<MessageResponse> generateUsers(@RequestParam("amount") int amount) {
+    @PostMapping("/users")
+    public ResponseEntity<MessageResponse> generateUsers(
+            @RequestParam(name = "amount", defaultValue = "10", required = false) int amount) {
         userGeneratorService.generateUsers(amount);
-        return ResponseEntity.ok(new MessageResponse("200", "Users generated successfully"));
+        return ResponseEntity.ok(new MessageResponse(
+                String.valueOf(HttpStatus.OK), amount + " users have been generated successfully"));
     }
 
-    @GetMapping("/products")
-    @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<MessageResponse> generateProducts(@RequestParam("amount") int amount) {
+    @PostMapping("/products")
+    public ResponseEntity<MessageResponse> generateProducts(
+            @RequestParam(name = "amount", defaultValue = "10", required = false) int amount) {
         productGeneratorService.generateProducts(amount);
-        return ResponseEntity.ok(new MessageResponse("200", "Products generated successfully"));
+        return ResponseEntity.ok(new MessageResponse(
+                String.valueOf(HttpStatus.OK), amount + " products have been generated successfully"));
     }
-// TODO: implement service method when OrderService is ready
-//    @GetMapping("/orders")
-//    public ResponseEntity<MessageResponse> generateOrders() {
-//        generatorService.generateOrders();
-//        return ResponseEntity.ok(new MessageResponse("200", "Orders generated successfully"));
-//    }
 }
 
